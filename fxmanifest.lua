78e674f168724bb0d2722870f678cfc2d2d779fb
Twiitchter <andylutze6090@gmail.com>
2021-10-19 16:23:02 +1100

    fix: missing conf in manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,14 +3,14 @@ fx_version "cerulean"
 game "gta5"
 author "Twiitchter"
 description 'c = exports["ig.core"]:c()'
-version "0.7.1"
+version "0.7.2"
 ------------------------------------------------------------------------------
 ui_page "nui/ig.core.html"
 loadscreen "https://www.ingenium.games/"
 loadscreen_cursor 'yes'
 ------------------------------------------------------------------------------
 -- shared
-shared_scripts {"conf.lua", "conf.default.lua", "conf.cars.lua", "conf.disable.lua", "conf.file.lua", "shared/_c.lua"}
+shared_scripts {"conf.lua", "conf.default.lua", "conf.cars.lua", "conf.disable.lua", "conf.file.lua", "conf.peds.lua", "shared/_c.lua"}
 ------------------------------------------------------------------------------
 -- client
 client_scripts {"client/_var.lua", "shared/[Tools]/*.lua", "shared/[Credits]/*.lua", "shared/[Data]/_meta.lua", "shared/[Data]/*.lua",
