edf8f81df7fd93d97d34c915c1bef5886672634f
Twiitchter <andylutze6090@gmail.com>
2021-11-13 21:12:58 +1100

    test

--- shared/[Data]/_items.lua
+++ shared/[Data]/_items.lua
@@ -79,9 +79,9 @@ Ok so this is the block of code for all you people to copy, paste, and prefill a
 -- ====================================================================================--
 
 local weapon_dump = exports["ig.dump"].GetWeapons()
-c.debug_1(c.table.Dump(weapon_dump))
+print(c.table.Dump(weapon_dump))
 
-c.debug_1(c.table.Dump(exports["ig.dump"].GetWeapons()))
+print(c.table.Dump(exports["ig.dump"].GetWeapons()))
 
 
 c.items = { -- table of items
