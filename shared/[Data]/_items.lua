60d1ca0b46ff5ddc1eeca6e1f86d0817a38efa82
Twiitchter <andylutze6090@gmail.com>
2021-11-16 21:57:11 +1100

    All Melee weapons added.

--- shared/[Data]/_items.lua
+++ shared/[Data]/_items.lua
@@ -19,9 +19,9 @@ local weapon_dump = exports["ig.dump"].GetWeapons()
 
 c.items = { -- table of items
 
--- ====================================================================================--
--- Misc
--- ====================================================================================--
+    -- ====================================================================================--
+    -- Misc
+    -- ====================================================================================--
 
     ["Cash"] = {
         Name = "Cash",
@@ -50,13 +50,14 @@ c.items = { -- table of items
         Image = "Cash.png",
     },
 
--- ====================================================================================--
--- Weapons
--- ====================================================================================--
+    -- ====================================================================================--
+    -- Weapons
+    -- ====================================================================================--
 
     -- ====================================================================================--
     -- Melee
     -- ====================================================================================--
+    
     ["WEAPON_KNIFE"] = {
         Name = "Knife",
         Degrade = false,
@@ -251,6 +252,291 @@ c.items = { -- table of items
         Stackable = false,
         Hotkey = true,
         Image = "WEAPON_CROWBAR.png",
+    },
+    ["WEAPON_SWITCHBLADE"] = {
+        Name = "Switchblade",
+        Degrade = false,
+        DegradeRate = 0.0,
+        Quality = 100,
+        Quantity = 1,
+        Cost = 75,
+        Value = 120,
+        Weight = 3,
+        Weapon = "3756226112",
+        Meta = {
+            Ammo = false,
+            SerialNumber = false,
+            BatchNumber = false,
+            Crafted = false,
+            Registered = false,
+            About = "Kids play with these.",
+        },
+        Data = weapon_dump["3756226112"],
+        Craftable = true,
+        Recipe = true,
+        Materials = {{
+            ItemRequired = "Rubber",
+            Quantity = 1
+        }, {
+            ItemRequired = "Spring",
+            Quantity = 1
+        }, {
+            ItemRequired = "Scrap Metal",
+            Quantity = 2
+        }},
+        Stackable = false,
+        Hotkey = true,
+        Image = "WEAPON_SWITCHBLADE.png",
+    },
+    ["WEAPON_STONE_HATCHET"] = {
+        Name = "Stone Hatchet",
+        Degrade = false,
+        DegradeRate = 0.0,
+        Quality = 100,
+        Quantity = 1,
+        Cost = 150,
+        Value = 500,
+        Weight = 3,
+        Weapon = "940833800",
+        Meta = {
+            Ammo = false,
+            SerialNumber = false,
+            BatchNumber = false,
+            Crafted = false,
+            Registered = false,
+            About = "Rust, v2",
+        },
+        Data = weapon_dump["940833800"],
+        Craftable = true,
+        Recipe = true,
+        Materials = {{
+            ItemRequired = "Stick",
+            Quantity = 1
+        }, {
+            ItemRequired = "Rock",
+            Quantity = 1
+        }, {
+            ItemRequired = "Animal Parts",
+            Quantity = 1
+        }},
+        Stackable = false,
+        Hotkey = true,
+        Image = "WEAPON_STONE_HATCHET.png",
+    },
+    ["WEAPON_BOTTLE"] = {
+        Name = "Broken Bottle",
+        Degrade = false,
+        DegradeRate = 0.0,
+        Quality = 100,
+        Quantity = 1,
+        Cost = 75,
+        Value = 120,
+        Weight = 3,
+        Weapon = "4192643659",
+        Meta = {
+            Ammo = false,
+            SerialNumber = false,
+            BatchNumber = false,
+            Crafted = false,
+            Registered = false,
+            About = "Drunks and Bums love to fight with these.",
+        },
+        Data = weapon_dump["4192643659"],
+        Craftable = true,
+        Recipe = true,
+        Materials = {{
+            ItemRequired = "Rubber",
+            Quantity = 1
+        }, {
+            ItemRequired = "Spring",
+            Quantity = 1
+        }, {
+            ItemRequired = "Scrap Metal",
+            Quantity = 2
+        }},
+        Stackable = false,
+        Hotkey = true,
+        Image = "WEAPON_BOTTLE.png",
+    },
+    ["WEAPON_BATTLEAXE"] = {
+        Name = "Battleaxe",
+        Degrade = false,
+        DegradeRate = 0.0,
+        Quality = 100,
+        Quantity = 1,
+        Cost = 435,
+        Value = 650,
+        Weight = 3,
+        Weapon = "4192643659",
+        Meta = {
+            Ammo = false,
+            SerialNumber = false,
+            BatchNumber = false,
+            Crafted = false,
+            Registered = false,
+            About = "There is roleplaying, then there is DnD",
+        },
+        Data = weapon_dump["4192643659"],
+        Craftable = true,
+        Recipe = true,
+        Materials = {{
+            ItemRequired = "Metal Rod",
+            Quantity = 1
+        }, {
+            ItemRequired = "Scrap Metal",
+            Quantity = 4
+        }, {
+            ItemRequired = "Carbon Alloy",
+            Quantity = 2
+        }, {
+            ItemRequired = "Rubber",
+            Quantity = 1
+        }},
+        Stackable = false,
+        Hotkey = true,
+        Image = "WEAPON_BATTLEAXE.png",
+    },
+    ["WEAPON_POOLCUE"] = {
+        Name = "Poolcue",
+        Degrade = false,
+        DegradeRate = 0.0,
+        Quality = 100,
+        Quantity = 1,
+        Cost = 135,
+        Value = 250,
+        Weight = 3,
+        Weapon = "2484171525",
+        Meta = {
+            Ammo = false,
+            SerialNumber = false,
+            BatchNumber = false,
+            Crafted = false,
+            Registered = false,
+            About = "A friendly game until it's not.",
+        },
+        Data = weapon_dump["2484171525"],
+        Craftable = true,
+        Recipe = true,
+        Materials = {{
+            ItemRequired = "Stick",
+            Quantity = 1
+        }, {
+            ItemRequired = "Paint",
+            Quantity = 1
+        }, {
+            ItemRequired = "Sand Paper",
+            Quantity = 2
+        }, {
+            ItemRequired = "Rubber",
+            Quantity = 1
+        }},
+        Stackable = false,
+        Hotkey = true,
+        Image = "WEAPON_POOLCUE.png",
+    },
+    ["WEAPON_WRENCH"] = {
+        Name = "Wrench",
+        Degrade = false,
+        DegradeRate = 0.0,
+        Quality = 100,
+        Quantity = 1,
+        Cost = 135,
+        Value = 250,
+        Weight = 3,
+        Weapon = "419712736",
+        Meta = {
+            Ammo = false,
+            SerialNumber = false,
+            BatchNumber = false,
+            Crafted = false,
+            Registered = false,
+            About = ".",
+        },
+        Data = weapon_dump["419712736"],
+        Craftable = true,
+        Recipe = true,
+        Materials = {},
+        Stackable = false,
+        Hotkey = true,
+        Image = "WEAPON_WRENCH.png",
+    },
+    ["WEAPON_DAGGER"] = {
+        Name = "Dagger",
+        Degrade = false,
+        DegradeRate = 0.0,
+        Quality = 100,
+        Quantity = 1,
+        Cost = 135,
+        Value = 250,
+        Weight = 3,
+        Weapon = "2460120199",
+        Meta = {
+            Ammo = false,
+            SerialNumber = false,
+            BatchNumber = false,
+            Crafted = false,
+            Registered = false,
+            About = ".",
+        },
+        Data = weapon_dump["2460120199"],
+        Craftable = true,
+        Recipe = true,
+        Materials = {},
+        Stackable = false,
+        Hotkey = true,
+        Image = "WEAPON_DAGGER.png",
+    },
+    ["WEAPON_MACHETE"] = {
+        Name = "Dagger",
+        Degrade = false,
+        DegradeRate = 0.0,
+        Quality = 100,
+        Quantity = 1,
+        Cost = 135,
+        Value = 250,
+        Weight = 3,
+        Weapon = "3713923289",
+        Meta = {
+            Ammo = false,
+            SerialNumber = false,
+            BatchNumber = false,
+            Crafted = false,
+            Registered = false,
+            About = ".",
+        },
+        Data = weapon_dump["3713923289"],
+        Craftable = true,
+        Recipe = true,
+        Materials = {},
+        Stackable = false,
+        Hotkey = true,
+        Image = "WEAPON_MACHETE.png",
+    },
+    ["WEAPON_HATCHET"] = {
+        Name = "Dagger",
+        Degrade = false,
+        DegradeRate = 0.0,
+        Quality = 100,
+        Quantity = 1,
+        Cost = 135,
+        Value = 250,
+        Weight = 3,
+        Weapon = "4191993645",
+        Meta = {
+            Ammo = false,
+            SerialNumber = false,
+            BatchNumber = false,
+            Crafted = false,
+            Registered = false,
+            About = ".",
+        },
+        Data = weapon_dump["4191993645"],
+        Craftable = true,
+        Recipe = true,
+        Materials = {},
+        Stackable = false,
+        Hotkey = true,
+        Image = "WEAPON_HATCHET.png",
     },
 
     -- ====================================================================================--
