197c31bacb97543c4087d6b5c00c4e38f1d72612
Twiitchter <andylutze6090@gmail.com>
2021-12-03 19:25:30 +1100

    test-remove tonumber on source pull

--- server/[State]/_player.lua
+++ server/[State]/_player.lua
@@ -10,42 +10,42 @@ c.cookies.player = {}
 --
 
 c.cookies.player.IsWanted = AddStateBagChangeHandler('IsWanted', nil, function(bagName, key, value, _, _)
-    local src = tonumber(bagName:gsub('player:', ''))
+    local src = bagName:gsub('player:', '')
     TriggerClientEvent("Client:RunChecks:IsWanted", src)
 end)
 --
 
 c.cookies.player.IsSupporter = AddStateBagChangeHandler('IsSupporter', nil, function(bagName, key, value, _, _)
-    local src = tonumber(bagName:gsub('player:', ''))
+    local src = bagName:gsub('player:', '')
     TriggerClientEvent("Client:RunChecks:IsSupporter", src)
 end)
 --
 
 c.cookies.player.IsDead = AddStateBagChangeHandler('IsDead', nil, function(bagName, key, value, _, _)
-    local src = tonumber(bagName:gsub('player:', ''))
+    local src = bagName:gsub('player:', '')
     TriggerClientEvent("Client:RunChecks:IsDead", src)
 end)
 --
 
 c.cookies.player.IsCuffed = AddStateBagChangeHandler('IsCuffed', nil, function(bagName, key, value, _, _)
-    local src = tonumber(bagName:gsub('player:', ''))
+    local src = bagName:gsub('player:', '')
     TriggerClientEvent("Client:RunChecks:IsCuffed", src)
 end)
 -- 
 
 c.cookies.player.IsEscorted = AddStateBagChangeHandler('IsEscorted', nil, function(bagName, key, value, _, _)
-    local src = tonumber(bagName:gsub('player:', ''))
+    local src = bagName:gsub('player:', '')
     TriggerClientEvent("Client:RunChecks:IsEscorted", src)
 end)
 --    
 
 c.cookies.player.IsEscorting = AddStateBagChangeHandler('IsEscorting', nil, function(bagName, key, value, _, _)
-    local src = tonumber(bagName:gsub('player:', ''))
+    local src = bagName:gsub('player:', '')
     TriggerClientEvent("Client:RunChecks:IsEscorting", src)
 end)
 --
 
 c.cookies.player.IsSwimming = AddStateBagChangeHandler('IsSwimming', nil, function(bagName, key, value, _, _)
-    local src = tonumber(bagName:gsub('player:', ''))
+    local src = bagName:gsub('player:', '')
     TriggerClientEvent("Client:RunChecks:IsSwimming", src)
 end)
