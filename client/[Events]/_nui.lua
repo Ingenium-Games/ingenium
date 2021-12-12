e33786f285b8ad2aed288a00adb18b20065f2e61
Andrew <andylutze6090@Gmail.com>
2021-12-12 20:14:29 +1100

    test

--- client/[Events]/_nui.lua
+++ client/[Events]/_nui.lua
@@ -8,8 +8,15 @@ AddEventHandler("Client:Core:UI", function(M, D)
         message = M,
         data = D
     })
-    SetNuiFocus(true, true)
+    
     if M == "Joining" then
+        DoScreenFadeOut(1000)
+        c.IsBusyPleaseWait(1000)
         ShutdownLoadingScreenNui()
+        DoScreenFadeIn(1000)
+        c.IsBusyPleaseWait(1000)
+        SetNuiFocus(true, true)
+    else
+        SetNuiFocus(true, true)
     end
 end)
