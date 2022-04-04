dbe6781831d6180c0bfa02233921d973028278b4
Twiitchter <andylutze6090@gmail.com>
2022-04-04 19:44:21 +1000

    Mistakes were made...

--- server/[Events]/_inventory.lua
+++ server/[Events]/_inventory.lua
@@ -4,6 +4,20 @@
 -- ====================================================================================--
 --  Get Character Info for the NUI to allow character selection.
 -- [C+S]
-AddEventHandler("Inventory:Consume:%s", function(quantity, position) 
-
-end)
+for k,v in pairs (c.items) do
+    AddEventHandler(("Inventory:Consume:%s"):format(v), function(quantity, position) 
+        local src = source
+        local quantity = quantity or 1
+        local postition = position
+        --
+        if c.item[v].weapon == true then
+            TriggerClientCallback({
+                source = src,
+                eventName = "Client:Equip:Weapon",
+                args = {v}
+            })
+        end
+        --
+        
+    end)
+end
