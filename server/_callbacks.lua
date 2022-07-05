f4cda4599e69b9c29f00130b0b856ad32674abab
Andrew <andylutze6090@Gmail.com>
2022-07-05 14:33:30 +1000

    updates

--- server/_callbacks.lua
+++ server/_callbacks.lua
@@ -35,3 +35,26 @@ local UseItem = RegisterServerCallback({
         
     end
 })
+
+-- ====================================================================================--
+
+local cb = RegisterServerCallback({
+    eventName = "GetDump",
+    eventCallback = function(source, dump)
+        if dump == "peds" then
+            return exports["ig.dump"]:GetPeds()
+        elseif dump == "tattoos" then
+            return exports["ig.dump"]:GetTattoos()
+        elseif dump == "zones" then
+            return exports["ig.dump"]:GetZones()
+        elseif dump == "weapons" then
+            return exports["ig.dump"]:GetWeapons()
+        elseif dump == "vehicles" then
+            return exports["ig.dump"]:GetVehicles()
+        elseif dump == "vehiclemodkits" then
+            return exports["ig.dump"]:GetVehicleModKits()
+        elseif dump == "cctvs" then
+            return exports["ig.dump"]:GetCCTV()
+        end    
+    end
+})
