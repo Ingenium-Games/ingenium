a16fa7651e90c26c8e0dfdcba569015614383b2e
Andrew <andylutze6090@Gmail.com>
2022-07-08 10:33:15 +1000

    comma

--- server/[SQL]/_character.lua
+++ server/[SQL]/_character.lua
@@ -13,7 +13,7 @@ NOTES.
 -- SHould remake htis one..
 function c.sql.char.Add(t, cb)
     MySQL.Async.execute(
-        "INSERT INTO `characters` (`Primary_ID`, `Character_ID`, `City_ID`, `First_Name`, `Last_Name`, `Height`, `Birth_Date`, `Iban` `Phone`, `Coords`, `Accounts`, `Modifiers`, `Appearance`) VALUES (@Primary_ID, @Character_ID, @City_ID, @First_Name, @Last_Name, @Height, @Birth_Date, @Iban, @Phone, @Coords, @Accounts, @Modifiers, @Appearance);",
+        "INSERT INTO `characters` (`Primary_ID`, `Character_ID`, `City_ID`, `First_Name`, `Last_Name`, `Height`, `Birth_Date`, `Iban`, `Phone`, `Coords`, `Accounts`, `Modifiers`, `Appearance`) VALUES (@Primary_ID, @Character_ID, @City_ID, @First_Name, @Last_Name, @Height, @Birth_Date, @Iban, @Phone, @Coords, @Accounts, @Modifiers, @Appearance);",
         {
             Primary_ID = t.Primary_ID,
             Character_ID = t.Character_ID,
