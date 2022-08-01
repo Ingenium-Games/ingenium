768b65bb8024a3ff7642ae220bcfd6a4e256e368
Andrew <andylutze6090@Gmail.com>
2022-08-01 18:22:27 +1000

    bag change handlers

--- server/[Onesync]/_sbch.lua
+++ server/[Onesync]/_sbch.lua
@@ -12,12 +12,22 @@ c.sbch.player = {}
 c.sbch.player.IsWanted = AddStateBagChangeHandler("IsWanted", nil, function(bagName, key, value, _, _)
     local src = bagName:gsub("player:", "")
     TriggerClientEvent("Client:RunChecks:IsWanted", src)
+    --
+    local xPlayer = c.data.GetPlayer(src)
+    if xPlayer then
+        
+    end
 end)
 --
 
 c.sbch.player.IsSupporter = AddStateBagChangeHandler("IsSupporter", nil, function(bagName, key, value, _, _)
     local src = bagName:gsub("player:", "")
     TriggerClientEvent("Client:RunChecks:IsSupporter", src)
+    --
+    local xPlayer = c.data.GetPlayer(src)
+    if xPlayer then
+        
+    end
 end)
 --
 
@@ -26,8 +36,10 @@ c.sbch.player.IsDead = AddStateBagChangeHandler("IsDead", nil, function(bagName,
     TriggerClientEvent("Client:RunChecks:IsDead", src)
     --
     local xPlayer = c.data.GetPlayer(src)
-    if value == false then
-        c.sql.char.SetDead(xPlayer.GetCharacter_ID(), false, {RevivedAt = c.func.Timestamp()})
+    if xPlayer then
+        if value == false then
+            c.sql.char.SetDead(xPlayer.GetCharacter_ID(), false, {RevivedAt = c.func.Timestamp()})
+        end
     end
 end)
 --
@@ -35,24 +47,44 @@ end)
 c.sbch.player.IsCuffed = AddStateBagChangeHandler("IsCuffed", nil, function(bagName, key, value, _, _)
     local src = bagName:gsub("player:", "")
     TriggerClientEvent("Client:RunChecks:IsCuffed", src)
+    --
+    local xPlayer = c.data.GetPlayer(src)
+    if xPlayer then
+        
+    end
 end)
 -- 
 
 c.sbch.player.IsEscorted = AddStateBagChangeHandler("IsEscorted", nil, function(bagName, key, value, _, _)
     local src = bagName:gsub("player:", "")
     TriggerClientEvent("Client:RunChecks:IsEscorted", src)
+    --
+    local xPlayer = c.data.GetPlayer(src)
+    if xPlayer then
+        
+    end
 end)
 --    
 
 c.sbch.player.IsEscorting = AddStateBagChangeHandler("IsEscorting", nil, function(bagName, key, value, _, _)
     local src = bagName:gsub("player:", "")
     TriggerClientEvent("Client:RunChecks:IsEscorting", src)
+    --
+    local xPlayer = c.data.GetPlayer(src)
+    if xPlayer then
+        
+    end
 end)
 --
 
 c.sbch.player.IsSwimming = AddStateBagChangeHandler("IsSwimming", nil, function(bagName, key, value, _, _)
     local src = bagName:gsub("player:", "")
     TriggerClientEvent("Client:RunChecks:IsSwimming", src)
+    --
+    local xPlayer = c.data.GetPlayer(src)
+    if xPlayer then
+        
+    end
 end)
 --
 
@@ -62,12 +94,22 @@ end)
 c.sbch.player.Bank = AddStateBagChangeHandler("Bank", nil, function(bagName, key, value, _, _)
     local src = bagName:gsub("player:", "")
     TriggerClientEvent("Client:RunChecks:Bank", src)
+    --
+    local xPlayer = c.data.GetPlayer(src)
+    if xPlayer then
+        
+    end
 end)
 --
 -- BANKING
 c.sbch.player.Cash = AddStateBagChangeHandler("Cash", nil, function(bagName, key, value, _, _)
     local src = bagName:gsub("player:", "")
     TriggerClientEvent("Client:RunChecks:Cash", src)
+    --
+    local xPlayer = c.data.GetPlayer(src)
+    if xPlayer then
+        
+    end
 end)
 --
 
