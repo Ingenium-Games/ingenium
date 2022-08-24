9e6d6c08003519e8b9be21b087ecf69886dbe430
Twiitchter <andylutze6090@gmail.com>
2022-08-24 15:22:17 +1000

    test

--- client/[Threads]/_weapon.lua
+++ client/[Threads]/_weapon.lua
@@ -5,21 +5,19 @@ Citizen.CreateThread(function()
         local ped = PlayerPedId()
         Citizen.Wait(0)
         if IsPedArmed(ped, 4 | 2) then
-            if IsControlPressed(0, 92) or IsControlPressed(0, 24) or IsControlPressed(0, 257) then
+            if IsPedShooting(ped) then
                 if c._weapon ~= nil and c._ammotype ~= nil then
                     c._ammo[c._ammotype] = c._ammo[c._ammotype] - 1
                 end
+                Citizen.Wait(115)
             end
             if IsPedReloading(ped) then
-                if c._ammo[c._ammotype] < 0 then
-                    c._ammo[c._ammotype] = 0
-                end
                 if c._weapon ~= nil then
                     TriggerServerCallback({
                         eventName = "UpdateAmmo",
                         args = {c._ammotype, c._ammo[c._ammotype]}
                     })
-                    Citizen.Wait(2000)
+                    Citizen.Wait(1250)
                 end
             end
         end
@@ -35,6 +33,22 @@ Citizen.CreateThread(function()
             DisableControlAction(1, 140, true)
             DisableControlAction(1, 141, true)
             DisableControlAction(1, 142, true)
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        local ped = PlayerPedId()
+        Citizen.Wait(0)
+        if IsPedArmed(ped, 4 | 2) then
+            if c._weapon ~= nil then
+                TriggerServerCallback({
+                    eventName = "UpdateAmmo",
+                    args = {c._ammotype, c._ammo[c._ammotype]}
+                })
+                Citizen.Wait(2500)
+            end
         end
     end
 end)
