37825873dcecaf688daba58c0181aea8974a4858
Twiitchter <you@example.com>
2022-12-08 23:43:51 +1100

    refactoring NUI

--- client/_nui.lua
+++ client/_nui.lua
@@ -26,3 +26,13 @@ RegisterNUICallback("__Register", function(Data, cb)
     TriggerServerEvent("Server:Character:Register", Data.First_Name, Data.Last_Name, appearance)
     cb("ok")
 end)
+
+RegisterNUICallback("__close", function(Data, cb)
+    -- Check if player is loaded as acharacter, otherwise dont disable nui, itll be in the character seleciton screen.
+    if c.data.IsPlayerLoaded() then
+        SetNuiFocus(false, false)
+        cb({message = "ok"})
+    else
+        cb({error = "__close called with no loaded character" })
+    end
+end)
